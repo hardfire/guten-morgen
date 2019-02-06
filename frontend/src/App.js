4f5c001c7289c802880699b20d1fd82f70dd7004
Avinash <avinash@avinash.com.np>
2019-02-06 14:52:25 +0545

    interim commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,38 @@ import Title from "./Title"
 import Movie from "./Movie"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/index.php/wp-json/wp/v2/movie/")
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          movies: result
+        })
+      }, error => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      })
+  }
+
   render() {
+    const { error, isLoaded, movies } = this.state
+    if(isLoaded === false) {
+      return "Loading"
+    }
+
     const movie = {
       id: 10,
       name: "Andaz Apna Apna",
